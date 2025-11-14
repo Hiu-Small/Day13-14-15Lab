91cb7251f2fc071e97b3b4965a1b775576c37944
hiusmall <trunghieunguyen443@gmail.com>
2025-11-14 15:34:18 +0700

    Day13Lab

---
+++ Bai_Thuc_Hanh_4/Bai_Thuc_Hanh_4/Models/Major.cs
@@ -0,0 +1,13 @@
+﻿namespace Bai_Thuc_Hanh_4.Models
+{
+    public class Major
+    {
+        public Major()
+        {
+            Learners = new HashSet<Learner>();
+        }
+        public int MajorID { get; set; }
+        public string MajorName { get; set; }
+        public virtual ICollection<Learner> Learners { get; set; }
+    }
+}
---
+++ Bai_Thuc_Hanh_4/Bai_Thuc_Hanh_4/Program.cs
@@ -0,0 +1,38 @@
+﻿using Bai_Thuc_Hanh_4.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+//builder.Configuration
+//    .SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("Secrets.json");
+//Đăng ký SchoolContext là một DbContext của ứng dụng
+builder.Services.AddDbContext<SchoolContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SchoolContext")));
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    DbInitializer.Initialize(services);
+}
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
