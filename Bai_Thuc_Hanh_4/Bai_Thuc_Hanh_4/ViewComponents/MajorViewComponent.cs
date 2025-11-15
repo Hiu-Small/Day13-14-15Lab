0657db56b7e3e860f16914edbc99f8ce8375a249
hiusmall <trunghieunguyen443@gmail.com>
2025-11-15 15:02:25 +0700

    Day14Lab

---
+++ Bai_Thuc_Hanh_4/Bai_Thuc_Hanh_4/ViewComponents/MajorViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Bai_Thuc_Hanh_4.Data;
+using Bai_Thuc_Hanh_4.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bai_Thuc_Hanh_4.ViewComponents
+{
+    public class MajorViewComponent : ViewComponent
+    {
+        SchoolContext db;
+        List<Major> majors;
+        public MajorViewComponent(SchoolContext _context)
+        {
+            db = _context;
+            majors = db.Majors.ToList();
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View("RenderMajor", majors);
+        }
+    }
+}
--- Bai_Thuc_Hanh_4/Bai_Thuc_Hanh_4/obj/Debug/net8.0/Bai_Thuc_Hanh_4.AssemblyInfo.cs
+++ Bai_Thuc_Hanh_4/Bai_Thuc_Hanh_4/obj/Debug/net8.0/Bai_Thuc_Hanh_4.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bai_Thuc_Hanh_4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91cb7251f2fc071e97b3b4965a1b775576c37944")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bai_Thuc_Hanh_4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bai_Thuc_Hanh_4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
